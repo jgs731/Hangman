fc8ccf6255efab3a51840eed15d3877aa11f4bb0
Joshua Smith <jgs73@live.co.uk>
2022-02-26 14:55:30 +0000

    Initial project setup - Added `selectRandomWordAndHide` method that selects a random word from the list and replaces characters with underscores - Wrote psuedo-code for the `evaluateCharacter` method, passing in character from user input - TODO: flesh out `evaluateCharacter` method - Created `HangmanGame` class to store methods in - Referenced in `Main` class to test that the method gives the expected output

--- Program.cs
+++ Program.cs
@@ -4,9 +4,44 @@ namespace Hangman
 {
     internal class Program
     {
+        public class HangmanGame
+        {
+            int remainingGuesses = 4;
+            Random random = new Random();
+            public string selectRandomWordAndHide()
+            {
+                var wordsToGuess = new List<string>() { "bottle", "orange", "radar", "add" };
+                string wordToBlank = wordsToGuess[random.Next(0, 5)];
+                string blankedWord = "";
+                for (int i = 0; i < wordToBlank.Length; i++)
+                {
+                    blankedWord += (wordToBlank[i].ToString().Replace(wordToBlank[i].ToString(), "_  "));
+                }
+                Console.Write(blankedWord);
+                return blankedWord;
+            }
+
+            public string evaluateCharacter(string guess)
+            {
+                // if character exists in string
+                // replace blank tile(s) with actual character
+                // else 
+                // Inform player that it doesn't exist
+                // Draw the hangman
+                // Decrement guesses remaining
+                return "";
+            }
+
+
+        }
+        
         static void Main(string[] args)
         {
-            
+            HangmanGame hmg = new HangmanGame();
+            hmg.selectRandomWordAndHide();
+
+            Console.WriteLine($"Enter a letter: {hmg.selectRandomWordAndHide()}");
+            string guessLetter = Console.ReadLine();
         }
     }
 }
