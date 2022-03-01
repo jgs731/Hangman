ca7489df72fdc5eee16fbc1487d2635b0dfae093
Joshua Smith <jgs73@live.co.uk>
2022-03-01 18:53:41 +0000

    Restructured project code - Removed `HangmanGame` class and enclosed methods - Moved body of methods into Main class - Renamed variables accordingly - Changed max value for `random.Next()` to use `.Count` instead of `.ToString.Length` - TODO: game state (need a while-loop?) - TODO: iterate through each character - behaviour - TODO: replace correctly guessed letter and represent this to the user

--- Program.cs
+++ Program.cs
@@ -4,50 +4,36 @@ namespace Hangman
 {
     internal class Program
     {
-        public class HangmanGame
-        {
-            int remainingGuesses = 4;
-            
-            public string WordToBlank { get; set; }
-
-            public string selectRandomWordAndHide()
-            {
-                string blankedWord = "";
-                for (int i = 0; i < WordToBlank.Length; i++)
-                {
-                    blankedWord += (WordToBlank[i].ToString().Replace(WordToBlank[i].ToString(), "_  "));
-                }
-                return blankedWord;
-            }
-
-            public void evaluateCharacter(string word, string guess)
-            {
-                for (int i = 0; i < word.Length; i++)
-                    if (word.Contains(guess))
-                    {
-                        Console.WriteLine("Exists");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Doesn't Exist");
-                        remainingGuesses--;
-                        Thread.Sleep(1000);
-                        Console.WriteLine("Draw line here");
-                    }
-            }
-        }
-        
         static void Main(string[] args)
         {
-            HangmanGame hmg = new HangmanGame();
             Random random = new Random();
             List<string> wordsToGuess = new List<string>() { "BOTTLE", "ORANGE", "RADAR", "ADD" };
+            int remainingGuesses = 4;
+            string wordToBlank = wordsToGuess[random.Next(0, wordsToGuess.Count)];
+            string blankedWord = "";
 
-            hmg.WordToBlank = wordsToGuess[random.Next(0, wordsToGuess.ToArray().Length)];
+            for (int i = 0; i < wordToBlank.Length; i++)
+            {
 
-            Console.WriteLine($"Enter a letter: {hmg.selectRandomWordAndHide()}");
+                blankedWord += (wordToBlank[i].ToString().Replace(wordToBlank[i].ToString(), "_  "));
+            }
+
+            Console.WriteLine($"Enter a letter: {blankedWord}");
             string guessLetter = Console.ReadLine();
-            hmg.evaluateCharacter(hmg.WordToBlank, guessLetter.ToUpper());
+            for (int i = 0; i < wordToBlank.Length; i++)
+            {
+                if (wordToBlank.Contains(guessLetter))
+                {
+                    Console.WriteLine("Exists");
+                }
+                else
+                {
+                    Console.WriteLine("Doesn't Exist");
+                    remainingGuesses--;
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Draw line here");
+                }
+            }
         }
     }
 }
