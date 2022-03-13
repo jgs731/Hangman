0aa04abb3f254f61e76d6a9bb743a70bc9f41121
Joshua Smith <jgs73@live.co.uk>
2022-03-13 20:49:05 +0000

    Improved game appearance in console - Added `Console.ReadKey` to track the character entered in by the player - Removed unnecessary `ToString()` method call on Ln.29 - Added `Console.Clear()` to clear the screen after every correct/incorrect guess

--- Program.cs
+++ Program.cs
@@ -12,31 +12,34 @@ namespace Hangman
             int remainingGuesses = 4;
             string wordToBlank = wordsToGuess[random.Next(0, wordsToGuess.Count)];
             string blankedWord = "";
+            ConsoleKeyInfo guessLetter;
 
             for (int i = 0; i < wordToBlank.Length; i++) blankedWord += "_";
 
-            while (wordToBlank != blankedWord || remainingGuesses!= 0)
+            while (wordToBlank != blankedWord || remainingGuesses != 0)
             {
-                Console.WriteLine($"Enter a letter: {blankedWord}");
-                string guessLetter = Console.ReadLine().ToUpper();
-                if (wordToBlank.ToString().Contains(guessLetter))
+                Console.WriteLine($"Enter a letter: {blankedWord}\n");
+                guessLetter = Console.ReadKey();
+                if (wordToBlank.Contains(guessLetter.Key.ToString()))
                 {
                     for (int i = 0; i < blankedWord.Length; i++)
                     {
-                        if (guessLetter == wordToBlank[i].ToString() && blankedWord[i] == '_')
+                        if (guessLetter.Key.ToString() == wordToBlank[i].ToString() && blankedWord[i] == '_')
                         {
-                            Console.WriteLine("Exists");
-                            blankedWord = blankedWord.Remove(i, 1).Insert(i, guessLetter).ToString(); // Remove underscore character + insert correct character in its place
+                            Console.WriteLine("\nExists");
+                            blankedWord = blankedWord.Remove(i, 1).Insert(i, guessLetter.Key.ToString()); // Remove underscore character + insert correct character in its place
                         }
                     }
+                    Thread.Sleep(500);
+                    Console.Clear();
                 }
                 else
                 {
-                    Console.WriteLine("Doesn't Exist");
+                    Console.WriteLine("\nDoesn't Exist");
                     remainingGuesses--;
-
-                    Thread.Sleep(1000);
                     Console.WriteLine("Draw line here"); // Draw method to be implemented and called on this line
+                    Thread.Sleep(1500);
+                    Console.Clear();
                 }
                 if (wordToBlank == blankedWord)
                 {
